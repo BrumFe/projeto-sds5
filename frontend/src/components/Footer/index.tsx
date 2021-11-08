d8f55beb8240b520aa526b83cad1b1fec994deca
Felipe Brum <felipe11brum@gmail.com>
2021-11-08 17:57:41 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/BrumFe" target="_blank" rel="noreferrer">Felipe Brum</a></p>
+                <p className="text-light"><a href="https://www.linkedin.com/in/felipe-brum-4a2138196/" target="_blank" rel="noreferrer">LinkedIn</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
